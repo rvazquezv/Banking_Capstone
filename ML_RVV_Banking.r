c4cba39cad7592f94109be104736a6755d90c9db
Ruben <rvazquezv@gmail.com>
2021-10-02 20:49:38 +0200

    Added initial Rmarkdown file with the Introduction, and first exploration to source dataset

--- ML_RVV_Banking.r
+++ ML_RVV_Banking.r
@@ -28,13 +28,14 @@ if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 if(!require(rvest)) install.packages("rvest", repos = "http://cran.us.r-project.org")
 if(!require(httr)) install.packages("httr", repos = "http://cran.us.r-project.org")
+if(!require(genefilter)) install.packages("genefilter", repos = "http://cran.us.r-project.org")
 
 library(tidyverse)
 library(caret)
 library(data.table)
 library(rvest)
 library(httr)
-
+library(genefilter)
 
 ###############################################################################
 ###############################################################################
@@ -95,13 +96,28 @@ dw_files<-list.files(path=dw_dir)
 #Download n all files and save them into Input_file_n  tibbles
 for(i in (1:length(dw_files))){
 exp1 <- expression(paste("Input_file",as.character(i),sep="_"))                                  # Create name of the tibble expression
-exp2<- expression(read_delim(file=paste(dw_dir,dw_files[i],sep="/"),delim=";",col_names=TRUE))  # Create read_delim expression
+exp2<- expression(read_delim(file=paste(dw_dir,dw_files[i],sep="/"),delim=";",col_names=TRUE))   # Create read_delim expression
 z<-paste(eval(exp1),exp2,sep="<-")                                                               # Create assignation expression as a string
-eval(parse(text=z))                                                                             # Evaluate expression
+eval(parse(text=z))                                                                              # Evaluate expression
 }
 
 
 #rm(Input_file_1,Input_file_2,Input_file_3)
+
+
+
+###############################################################################
+###############################################################################
+##
+## Source Data exploratory analysis
+##
+###############################################################################
+###############################################################################
+
+
+
+
+
 
 #Confirming Input_file_3 is a subset of Input_file_1 while anti_join() return all rows from x without a match in y.
 # Thus if previous antijoin is 0 means one set is contained in the other one
@@ -109,6 +125,110 @@ nrow(anti_join(Input_file_3,Input_file_1,by=NULL))
 
 
 
+head(Input_file_1)
+summary(Input_file_1)
+
+
 head(Input_file_2)
 summary(Input_file_2)
 
+
+
+###############################################################################
+###############################################################################
+##
+## Partitioning source data
+##
+###############################################################################
+###############################################################################
+
+
+
+# Validation set will be 20% of Source data according to Pareto principle
+set.seed(1978, sample.kind="Rounding") # if using R 3.5 or earlier, use `set.seed(1)`
+test_index <- createDataPartition(y = Input_file_1$y, times = 1, p = 0.2, list = FALSE)
+bankraw<- Input_file_1[-test_index,]
+Validation <- Input_file_1[test_index,]
+
+
+
+head(bankraw)
+summary(bankraw)
+
+
+
+head(Validation)
+summary(Validation)
+
+
+
+###############################################################################
+##
+##        Preprocessing, cleaning, wrangling and creating partitions
+##        
+###############################################################################
+
+## Selecting a random seed to allow replicability
+set.seed(1978, sample.kind="Rounding")
+
+
+## Creating training a testing partitions on bankraw dataset 
+
+test_index <- createDataPartition(y = bankraw$y, times = 1, p = 0.2, 
+                                  list = FALSE)
+train_set <- bankraw[-test_index,]
+test_set <- bankraw[test_index,]
+
+
+
+str(train_set)
+
+#Build an index with all character columns in the dataset that will need to become factor
+idx_isc<-which(sapply(train_set,is.character)==TRUE)
+
+# Get the columns to be converted to factor and split from the others that do not need to change
+df1<-train_set[,idx_isn]
+df2<-train_set[,-idx_isn]
+
+l<-sapply(df1,factor)
+
+
+which(idx_isn==TRUE)
+
+for(i in (1:ncol(x))){
+  train_set[,i]<-ifelse(idx_isn[[i]],train_set[,i],factor(train_set[,i]))
+}  
+
+
+str(a)
+
+summary(train_set)
+summary(y)
+
+
+
+###############################################################################
+##
+##        Modelling
+##        I.) BASELINE
+###############################################################################
+
+
+
+tt<-colttests(train_set[,1:20],train_set[,21])
+
+x<-as.matrix(train_set[,1:20])
+y<-factor(train_set$y)
+
+tt <- colttests(x, y)
+
+tt$p.value
+
+ind <- which(tt$pvalue <= 0.01)
+
+
+train_set$job<-factor(train_set$job)
+
+
+
+fit <- lm(y ~ ., data = train_set)
